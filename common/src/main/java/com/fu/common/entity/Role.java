8e147c998fb0c6d3f8f7cb261a6a74b1ecbdd548
Meta39 <53995537@qq.com>
2022-05-16 10:48:03 +0800

    去掉多余import

--- common/src/main/java/com/fu/common/entity/Role.java
+++ common/src/main/java/com/fu/common/entity/Role.java
@@ -1,8 +1,5 @@
 package com.fu.common.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.util.Date;
 
 public class Role {
